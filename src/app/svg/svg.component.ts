536cd2f615a0be289f3b58d0191b842cd09c8462
pri <primoh1997@gmail.com>
2024-11-17 10:08:35 -0500

    change to proxy

--- src/app/svg/svg.component.ts
+++ src/app/svg/svg.component.ts
@@ -1,17 +1,17 @@
-import { Component, AfterViewInit, Output, EventEmitter, Renderer2, ElementRef } from '@angular/core';
+import { Component,  Output, EventEmitter, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-svg',
   templateUrl: './svg.component.html',
   styleUrls: ['./svg.component.scss']
 })
-export class SvgComponent implements AfterViewInit {
+export class SvgComponent  {
 
   @Output() send = new EventEmitter<string>();
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     const country = Array.from(this.el.nativeElement.querySelectorAll('path')) as SVGElement[]; // convert NodeList to array
 
     let current = '';
@@ -21,5 +21,5 @@ export class SvgComponent implements AfterViewInit {
         this.send.emit(current);
       });
     });
-  }
+  } 
 }
