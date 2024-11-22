985305e8d451ebf93d2665a21094a6487de46e4b
primo14 <164257594+primo14@users.noreply.github.com>
2024-11-21 19:32:52 -0500

    paramter fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: process.env.API_KEY,
-  apiEndpoint: process.env.API_ENDPOINT
+  apiKey: process.env['API_KEY'],
+  apiEndpoint: process.env['API_ENDPOINT']
 };
