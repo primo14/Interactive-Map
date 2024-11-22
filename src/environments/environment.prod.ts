fe823ceed6bd3837540774be67d70636f30443fd
primo14 <164257594+primo14@users.noreply.github.com>
2024-11-21 21:38:24 -0500

    fix environenment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiKey: process.env['API_KEY'],
-  apiEndpoint: process.env['API_ENDPOINT']
+  apiKey: 'your-api-key-value', // Replace with your actual API key
+  apiEndpoint: 'https://api.worldbank.org/v2' // Replace with your actual API endpoint
 };
